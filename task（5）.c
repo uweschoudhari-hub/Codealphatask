07a639f57b19573ac0da6ce02bf33a5f9da02195
uweschoudhari-hub <uweschoudhari@gmail.com>
2026-04-14 13:11:15 +0530

    Task 1

---
+++ task（5）.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("--- Simple C Calculator ---\n");
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '/':
+            // Check for division by zero
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error! Division by zero is undefined.\n");
+            }
+            break;
+
+        // Handles any input that isn't +, -, *, or /
+        default:
+            printf("Error! Operator is not correct.\n");
+    }
+
+    return 0;
+}
